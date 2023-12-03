8f93c3b5ef06da36fd4cd8068ef175202a90f5ad
kerm1t <wolfgang.schulz@gmail.com>
2023-12-03 10:01:13 +0100

    update for part2, which doesn't pass yet

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -5,35 +5,63 @@
 #include <fstream>
 #include <string>
 
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+  size_t start_pos = str.find(from);
+  if (start_pos == std::string::npos) return false;
+  str.replace(start_pos, from.length(), to);
+  return true;
+}
+
+void part1(bool b_numberz_as_string) {
+  std::ifstream f;
+  f.open("data.txt");
+  std::string line;
+  int sum = 0;
+  while (f) {
+    std::getline(f, line);
+    if (f) { // hmm, leider erforderlich
+      int num[2] = { 0, 0 };
+      int ndigits = 0;
+      std::cout << line << ": ";
+      if (b_numberz_as_string) {
+        while (replace(line, "one", "1"));
+        while (replace(line, "two", "2"));
+        while (replace(line, "three", "3"));
+        while (replace(line, "four", "4"));
+        while (replace(line, "five", "5"));
+        while (replace(line, "six", "6"));
+        while (replace(line, "seven", "7"));
+        while (replace(line, "eight", "8"));
+        while (replace(line, "nine", "9"));
+//        while (replace(line, "zero", "0"));
+// --------------------------------------------------
+// hmm, this method doesn't work with aliased numberz
+// e.g.      three49oneightf: 3491ightf : 31
+// instead it requires to e.g. copy all found numberz
+// --------------------------------------------------
+      }
+      for (auto i : line) {
+        if (isdigit(i)) {
+          if (ndigits > 0)
+            num[1] = i - '0'; // overwrite the second digit)
+          else
+            num[0] = i - '0';
+          ndigits++;
+        }
+      }
+      if (ndigits == 1) num[1] = num[0];
+      int nline = num[0] * 10 + num[1];
+      sum += nline;
+      std::cout << line << ": " << nline << '\n';//f.tellg() << '\n';
+    }
+  }
+  f.close();
+  std::cout << "sum = " << sum << '\n';
+}
+
 int main()
 {
-    std::ifstream f;
-    f.open("data.txt");
-    std::string line;
-    int sum = 0;
-    while (f) {
-      std::getline(f, line);
-      if (f) { // hmm, leider erforderlich
-        char buf[100];
-        int num[2] = { 0, 0 };
-        int ndigits = 0;
-        for (auto i:line) {
-          if (isdigit(i)) {
-            if (ndigits>0)
-              num[1] = i - '0'; // overwrite the second digit)
-            else
-              num[0] = i-'0';
-            ndigits++;
-          }
-        }
-        if (ndigits == 1) num[1] = num[0];
-        int nline = num[0] * 10 + num[1];
-        sum += nline;
-        std::cout << line << ": " << nline << '\n';//f.tellg() << '\n';
-      }
-    }
-    f.close();
-    std::cout << "sum = " << sum << '\n';
+  part1(true); // b_numberz_as_string
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
