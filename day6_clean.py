a8e0fe2ff59bc67425a193b4c1bf26d3cf3f0613
kerm1t <wolfgang.schulz@gmail.com>
2023-12-06 07:39:03 +0100

    part2 --> need GPU

--- day6_clean.py
+++ day6_clean.py
@@ -6,6 +6,7 @@ Created on Wed Dec  6 06:11:14 2023
 """
 import numpy as np
 
+# part 1
 Time  = [       61   ,  67 ,    75 ,    71 ]
 Distance = [   430 ,  1036 ,  1307 ,  1150 ]
 
@@ -30,3 +31,17 @@ print(np.prod(wins))
 # should have used the given examples as test set earlier :-)
 # acc is actually speed :-) ... changed it
 # use numpy from start
+
+# part 2 --> hmm, this a dumb abuse of electricity (in python) .. might by nice with a GPU
+Time  = [       61677571 ]
+Distance = [   430103613071150 ]
+race = 0
+wins = np.zeros(0)
+dst = np.zeros(Time[race])
+for speed in range(Time[race]):
+    for t in range(Time[race]-speed):
+       dst[speed] = dst[speed]+speed
+    if dst[speed] > Distance[race]:
+        wins[race] = wins[race]+1
+    print(wins)
+print(dst)
